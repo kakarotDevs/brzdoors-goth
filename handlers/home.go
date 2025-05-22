d0c52840f136009f7b57466ac57eb56a842f933c
jarrodbradshaw <jarrodbradshaw@pm.me>
2025-05-22 13:45:07 +1000

    Initial copy of brzdoors-goth for pre-launch backend branch

--- handlers/home.go
+++ handlers/home.go
@@ -11,3 +11,10 @@ func HandleHome(w http.ResponseWriter, r *http.Request) error {
 	slog.Info("Serving homepage")
 	return Render(w, r, home.Index())
 }
+
+func HandleClick(w http.ResponseWriter, r *http.Request) error {
+	slog.Info("HTMX click received")
+	w.Header().Set("Content-Type", "text/html")
+	_, err := w.Write([]byte(`<h1 class="text-2xl text-green-600">You clicked the button!</h1>`))
+	return err
+}
--- main.go
+++ main.go
@@ -18,21 +18,23 @@ func main() {
 		log.Fatal(err)
 	}
 
-	r := chi.NewMux()
+	router := chi.NewMux()
 
 	// Middleware should come before route declarations
-	r.Use(middleware.Recoverer)
+	router.Use(middleware.Recoverer)
 
-	r.Handle("/public/*", public())
+	router.Handle("/public/*", public())
 
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
-	r.Get("/", handlers.Make(handlers.HandleHome))
-	r.Get("/about", handlers.Make(handlers.HandleAbout))
-	r.Get("/contact", handlers.Make(handlers.HandleContact))
-	r.Get("/order", handlers.Make(handlers.HandleOrder))
+	router.Get("/", handlers.Make(handlers.HandleHome))
+	router.Get("/about", handlers.Make(handlers.HandleAbout))
+	router.Get("/contact", handlers.Make(handlers.HandleContact))
+	router.Get("/order", handlers.Make(handlers.HandleOrder))
+
+	router.Get("/clicked", handlers.Make(handlers.HandleClick))
 
 	listenAddr := os.Getenv("LISTEN_ADDR")
 	if listenAddr == "" {
@@ -40,5 +42,5 @@ func main() {
 	}
 
 	slog.Info("3, 2, 1, Let's Jam...", "url", fmt.Sprintf("http://localhost%s", listenAddr))
-	log.Fatal(http.ListenAndServe(listenAddr, r))
+	log.Fatal(http.ListenAndServe(listenAddr, router))
 }
